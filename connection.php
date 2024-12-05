2acc1e67b12812eebfe885c4fbd8d0c27d5c597a
dphuong <duyphuong.devg@gmail.com>
2024-12-05 22:36:31 +0700

    upload docker file

--- connection.php
+++ connection.php
@@ -1,17 +1,45 @@
 <?php
+// class DB
+// {
+//     public static $instance = NULL;
+//     public static function getInstance() 
+//     {
+//         if (!isset(self::$instance)) 
+//         {
+//             self::$instance = mysqli_connect("mysql", "root", "root", "assignWeb");
+//             if (mysqli_connect_error())
+//             {
+//                 die("Failed to connect to MySQL: " . mysqli_connect_error());
+//             }
+//         }
+//         return self::$instance;
+//     }
+// }
 class DB
 {
     public static $instance = NULL;
+
     public static function getInstance() 
     {
         if (!isset(self::$instance)) 
         {
-            self::$instance = mysqli_connect("mysql", "root", "root", "assignWeb");
-            if (mysqli_connect_error())
-            {
-                die("Failed to connect to MySQL: " . mysqli_connect_error());
+            $retries = 5;
+            while ($retries > 0) {
+                self::$instance = mysqli_connect("mysql", "root", "root", "assignWeb");
+                
+                if (self::$instance) {
+                    return self::$instance;
+                }
+
+                // Wait and retry
+                $retries--;
+                sleep(2); // Wait for 2 seconds before retrying
             }
+
+            // If connection fails after retries
+            die("Failed to connect to MySQL: " . mysqli_connect_error());
         }
         return self::$instance;
     }
 }
+?>
